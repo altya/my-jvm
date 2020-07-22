111ea098f99df01851a4299bb991883e778a28ae
xiaoming <844296970@qq.com>
2020-07-22 11:26:10 +0800

    添加测试结果

--- go/src/cn.hdudjm/my-jvm/main.go
+++ go/src/cn.hdudjm/my-jvm/main.go
@@ -1,7 +1,8 @@
 package main
 
 import "fmt"
-
+import "strings"
+import "cn.hdudjm/my-jvm/classpath"
 func main() {
 	cmd := parseCmd()
 
@@ -14,7 +15,18 @@ func main() {
 	}
 }
 
+
 func startJVM(cmd *Cmd) {
-	fmt.Printf("classpath:%s class:%s args:%v\n",
-		cmd.cpOption, cmd.class, cmd.args)
+	cp := classpath.Parse(cmd.XjreOption, cmd.cpOption)
+	fmt.Printf("classpath:%v class:%v args:%v\n",
+		cp, cmd.class, cmd.args)
+
+	className := strings.Replace(cmd.class, ".", "/", -1)
+	classData, _, err := cp.ReadClass(className)
+	if err != nil {
+		fmt.Printf("Could not find or load main class %s\n", cmd.class)
+		return
+	}
+
+	fmt.Printf("class data:%v\n", classData)
 }
