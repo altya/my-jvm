2eb4c1951aaee6d22aa2ce21da91e042913914d9
xiaoming <844296970@qq.com>
2020-07-25 09:56:38 +0800

    增加读取数据工具类的注释。

--- go/src/cn.hdudjm/my-jvm/classfile/class_reader.go
+++ go/src/cn.hdudjm/my-jvm/classfile/class_reader.go
@@ -6,21 +6,21 @@ type ClassReader struct {
 	data []byte
 }
 
-// u1
+// 截取一个字节的数据
 func (self *ClassReader) readUint8() uint8 {
 	val := self.data[0]
 	self.data = self.data[1:]
 	return val
 }
 
-// u2
+// 截取两个字节的数据
 func (self *ClassReader) readUint16() uint16 {
 	val := binary.BigEndian.Uint16(self.data)
 	self.data = self.data[2:]
 	return val
 }
 
-// u4
+// 使用BigEndian获取4字节数据
 func (self *ClassReader) readUint32() uint32 {
 	val := binary.BigEndian.Uint32(self.data)
 	self.data = self.data[4:]
@@ -42,6 +42,7 @@ func (self *ClassReader) readUint16s() []uint16 {
 	return s
 }
 
+// 读取n个字节数据
 func (self *ClassReader) readBytes(n uint32) []byte {
 	bytes := self.data[:n]
 	self.data = self.data[n:]
