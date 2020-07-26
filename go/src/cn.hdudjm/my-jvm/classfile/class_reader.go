5c0453330bf6722f44439ae48fac594a41cbdfd0
xiaoming <844296970@qq.com>
2020-07-26 22:39:09 +0800

    添加4中基本数据类型。

--- go/src/cn.hdudjm/my-jvm/classfile/class_reader.go
+++ go/src/cn.hdudjm/my-jvm/classfile/class_reader.go
@@ -32,7 +32,7 @@ func (self *ClassReader) readUint64() uint64 {
 	self.data = self.data[8:]
 	return val
 }
-
+//读取2字节数据【表】。【表】的概念类似于Java的数组。
 func (self *ClassReader) readUint16s() []uint16 {
 	n := self.readUint16()
 	s := make([]uint16, n)
